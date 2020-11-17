6f604444f6154f19babdec330b9fd0675b68a981
Jasmine Coder <jasminethecoder@gmail.com>
2020-11-16 21:14:56 -0800

    add one to many association between recipe and chef

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,4 +5,6 @@ class Chef < ApplicationRecord
     /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, 
         uniqueness: {case_sensitive: false}
+    
+    has_many :recipes
 end
