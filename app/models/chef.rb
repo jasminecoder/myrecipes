1218482e2902e9e14333901b18ff85fd4fd2f854
Jasmine Coder <jasminethecoder@gmail.com>
2020-12-02 13:12:16 -0800

    add Comment model and associations

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -10,4 +10,5 @@ class Chef < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
     default_scope -> {order(updated_at: :desc)}
+    has_many :comments, dependent: :destroy
 end
