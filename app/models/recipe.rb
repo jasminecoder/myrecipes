7b9d836ffd25f21a39400415c234a00740088f13
Jasmine Coder <jasminethecoder@gmail.com>
2020-12-05 18:59:32 -0800

    make comment real time

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
     validates :name, presence: true
+    before_save { self.name = name.titlecase }
     validates :description, presence: true, length: {minimum: 5, maximum: 500}
     belongs_to :chef
     validates :chef_id, presence: true
